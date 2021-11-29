aa4ed7c805e2cd40b5a8635dcc5d3f9dfec72b38
Treescrub <treescrub@gmail.com>
2021-11-29 14:00:14 -0700

    Fixed API error and changed timestamp type to epoch milliseconds

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import json
 
 app = Flask(__name__)
 
-db_connection = sqlite3.connect("sensor_data.db", detect_types=sqlite3.PARSE_DECLTYPES)
-db_cursor = db_connection.cursor()
-
 num_data_limit = 100
 
 @app.route("/")
@@ -15,11 +12,14 @@ def get_page():
 
 @app.route("/get_data")
 def get_data():
+    db_connection = sqlite3.connect("sensor_data.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    db_cursor = db_connection.cursor()
+
     data = {"pm25": [], "pm10": []}
-    for row in db_cursor.execute("SELECT * FROM PM25 ORDER BY timestamp ASC LIMIT :limit", {"limit": num_data_limit}):
+    for row in db_cursor.execute('SELECT timestamp AS "timestamp [timestamp]", value AS "value [real]" FROM PM25 ORDER BY timestamp ASC LIMIT :limit', {"limit": num_data_limit}):
         data["pm25"].append({"timestamp": row[0].timestamp() * 1000, "value": row[1]})
 
-    for row in db_cursor.execute("SELECT * FROM PM10 ORDER BY timestamp ASC LIMIT :limit", {"limit": num_data_limit}):
+    for row in db_cursor.execute('SELECT timestamp AS "timestamp [timestamp]", value AS "value [real]" FROM PM10 ORDER BY timestamp ASC LIMIT :limit', {"limit": num_data_limit}):
         data["pm10"].append({"timestamp": row[0].timestamp() * 1000, "value": row[1]})
 
     return json.dumps(data)
