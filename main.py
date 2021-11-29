6ed61dc0043f1e7721334bb30333371867fb1025
Treescrub <treescrub@gmail.com>
2021-11-29 12:22:33 -0700

    Created skeleton page and basic API

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+import json
 
 app = Flask(__name__)
 
+db_connection = sqlite3.connect("sensor_data.db", detect_types=sqlite3.PARSE_DECLTYPES)
+db_cursor = db_connection.cursor()
+
+num_data_limit = 100
+
 @app.route("/")
 def get_page():
-    return
+    return render_template("index.html")
+
+@app.route("/get_data")
+def get_data():
+    data = {"pm25": [], "pm10": []}
+    for row in db_cursor.execute("SELECT * FROM PM25 ORDER BY timestamp ASC LIMIT :limit", {"limit": num_data_limit}):
+        data["pm25"].append({"timestamp": row[0].timestamp(), "value": row[1]})
+
+    for row in db_cursor.execute("SELECT * FROM PM10 ORDER BY timestamp ASC LIMIT :limit", {"limit": num_data_limit}):
+        data["pm10"].append({"timestamp": row[0].timestamp(), "value": row[1]})
+
+    return json.dumps(data)
 
 def main():
     app.run(port=5000)
